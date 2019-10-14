acdb763c0e98e411249519bb43f9ffe1f5680f84
verywitted <philiphall@live.com>
2019-10-14 16:38:41 +0100

    include midi files

---
+++ mediapipe/midi/DetectionToMidi.cpp
@@ -0,0 +1,13 @@
+#include "DetectionToMidi.h"
+
+namespace mediapipe {
+
+class DetectionToMidi {
+
+	public:
+		DetectionToMidi(float x, float y) {
+			std::cout << x;
+			std::cout << y;
+}
+
+}
---
+++ mediapipe/midi/DetectionToMidi.h
@@ -0,0 +1,7 @@
+
+class DetectionToMidi {
+
+	public:
+		DetectionToMidi(float x, float y);
+		DetectionToMidi()=delete;
+};
