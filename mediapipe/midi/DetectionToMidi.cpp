175b9c1e6d9db91bf9724ee0b79c52c3797548e8
phil <phil@localhost.localdomain>
2019-10-16 13:08:27 +0100

    use glog to log message·

--- mediapipe/midi/DetectionToMidi.cpp
+++ mediapipe/midi/DetectionToMidi.cpp
@@ -1,14 +1,14 @@
 #include "DetectionToMidi.h"
-#include <android/log.h>
+#include "glog/logging.h"
 #define LOG_TAG "PIPE_POINTS"
-
+  
 namespace mediapipe {
 
 	class DetectionToMidi {
 	    DetectionToMidi(float x, float y) 
 		{
-__android_log_print(1, "HAND_x", "%f", x);
-__android_log_print(1, "HAND_y", "%f", y);
+			LOG(WARN) << "HAND_x = " << x;
+			LOG(WARN) << "HAND_y = " << y;
 		}
 	};
 
